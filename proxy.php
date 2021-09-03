046ed8fbfda74148eab575f4b9f4c269fa696ab2
Gavdsc <gav@gavsblog.com>
2021-09-03 15:05:31 +0100

    Headers/method overrides

--- proxy.php
+++ proxy.php
@@ -7,24 +7,37 @@ namespace GavsBlog;
  * Note: Proxy is only intended to be run once per request / @todo expand for re-use using curl_reset().
  * @todo Write a curl cookie cleanup script
  * @todo Other request methods
- * @todo Get/set
- * @todo Return head option
+ * @todo Redirect/route list
+ * @todo Setupd referrers (curl_setopt($this->ch, CURLOPT_REFERER, $address);
+ * @tood Header overrides
  * @package PathController
  */
 class Proxy {
-    private $ch;
+    private \CurlHandle $ch;
     private string $url;
-    private $cookie;
-    private string $response;
+    private string $cookie;
+    private string $method;
     private array $responseHeaders;
-    private array $requestHeaders;
+    private string $requestHeaders;
+    private string $response;
 
-    function __construct($url, $echo = false, $cookie = '') {
+    /**
+     * Proxy constructor
+     * @param $url
+     * @param false $echo
+     * @param string $cookie
+     * @param null $methodOverride
+     * @param null $headerOverride
+     */
+    function __construct($url, $echo = false, $cookie = '', $methodOverride = null, $headerOverride = null) {
         // Setup curl
         $this->ch = curl_init();
 
         // Set url assuming return transfer
         $this->setUrl($url);
+
+        // Set method (either use the request method or override)
+        $this->method = $methodOverride ? strtolower($methodOverride) : strtolower($_SERVER['REQUEST_METHOD']);
 
         // Setup cookie pot
         $this->cookie = $cookie;
@@ -42,7 +55,8 @@ class Proxy {
     }
 
     /**
-     * Function: Cleanup curl when no longer needed, feel free to unset()
+     * Proxy destructor
+     * Cleanup curl when no longer needed, feel free to unset()
      */
     function __destruct() {
         curl_close($this->ch);
@@ -54,6 +68,7 @@ class Proxy {
      */
     public function getHeaders() : array {
         return array(
+            "method" => $this->method,
             "request" => $this->requestHeaders,
             "response" => $this->responseHeaders
         );
@@ -68,10 +83,9 @@ class Proxy {
     }
 
     /**
-     *
+     * Function: Setup and run curl
      */
     public function run() {
-
         // Process method
         $this->processMethod();
 
@@ -84,8 +98,6 @@ class Proxy {
         // Catch response headers
         $headers = [];
 
-        curl_setopt($this->ch, CURLOPT_RETURNTRANSFER, 1);
-
         curl_setopt($this->ch, CURLOPT_HEADERFUNCTION,
             function($curl, $header) use (&$headers) {
 
@@ -93,7 +105,7 @@ class Proxy {
                 $header = explode(':', $header, 2);
 
                 if (count($header) < 2)
-                    return $len;
+                    return $length;
 
                 $headers[strtolower(trim($header[0]))][] = trim($header[1]);
 
@@ -103,30 +115,23 @@ class Proxy {
 
         curl_setopt($this->ch, CURLINFO_HEADER_OUT, true);
 
-        curl_setopt($this->ch, CURLOPT_REFERER, 'http://oculus:80');
-
-
         // Curl request
         $this->response = curl_exec($this->ch);
 
         // Save request and response headers
         $info = curl_getinfo($this->ch);
-
         $this->responseHeaders = $headers;
+
+        // Todo: check against header size / for 404 before setting
+        // Todo: convert string to array
         $this->requestHeaders = $info['request_header'];
-
-        //    var_dump($headers);
-
-        //   var_dump($info['request_header']);
-
-
-        // dump the headers
-        /*   $header_size = curl_getinfo($this->ch, CURLINFO_HEADER_SIZE);
-           $header = substr($this->response, 0, $header_size);
-
-           var_dump($header); */
     }
 
+    /**
+     * Function: Set request url with/without headers
+     * @param $url
+     * @param bool $transfer
+     */
     private function setUrl($url, $transfer = true) {
         // Set the url
         $this->url = $url;
@@ -138,9 +143,12 @@ class Proxy {
         }
     }
 
+    /**
+     * Function: Switch statement for processing delivery methods
+     */
     private function processMethod() {
         // @todo: Expand for other methods
-        switch (strtolower($_SERVER['REQUEST_METHOD'])) {
+        switch ($this->method) {
             case "post":
                 $this->buildPost();
                 break;
@@ -149,43 +157,48 @@ class Proxy {
                 break;
             case "get":
             default:
+                // Nothing extra needed
                 break;
         }
     }
 
+    /**
+     * Function: Build post options / get current post data from input buffer
+     */
     private function buildPost() {
         // Get post data from input buffer
         $postData = file_get_contents('php://input');
 
-        // Set json headers (for bi test) todo: intergrate properly
-        curl_setopt($this->ch, CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Origin: http://oculus:80',
-                'Access-Control-Request-Headers: content-type',
-                'Access-Control-Request-Method: POST'
-            )
-        );
+        // Set request content type for post (taken from incoming request / override in headerOverride if necessary)
+        if (isset($_SERVER['CONTENT_TYPE']) || isset($_SERVER['HTTP_CONTENT_TYPE'])) {
+            curl_setopt($this->ch, CURLOPT_HTTPHEADER, array(
+                    'Content-Type: ' . $_SERVER['HTTP_CONTENT_TYPE']
+                )
+            );
+        }
 
         curl_setopt($this->ch, CURLOPT_POST, true);
         curl_setopt($this->ch, CURLOPT_POSTFIELDS, $postData);
     }
 
+    /**
+     * Function: Build options request, mostly pre-flight stuff
+     */
     private function buildOptions() {
-        // Check and return CORS preflights
+        // Todo: Check and return CORS preflights
         curl_setopt($this->ch, CURLOPT_OPTIONS, true);
-
-        // Preflight CORS
-        curl_setopt($this->ch, CURLOPT_HTTPHEADER,
-            array(
-                'Content-Type: application/json',
-                'Origin: http://oculus:80',
-                'Access-Control-Request-Headers: content-type',
-                'Access-Control-Request-Method: POST'
-            )
-        );
     }
 
+    /**
+     * Function: Override current request headers (passed on construction)
+     */
+    private function headerOverride() {
+        // Todo: header overrides
+    }
+
+    /**
+     * Function: Setup cookie jar etc if cookies are enabled
+     */
     private function setCookies() {
         // Setup php session to handle cookies per individual
         $sessionId = $this->handleSession();
@@ -194,6 +207,10 @@ class Proxy {
         curl_setopt($this->ch, CURLOPT_COOKIEFILE, $this->cookie . "\\curl_$sessionId");
     }
 
+    /**
+     * Function: Save sessions
+     * @return false|string
+     */
     private function handleSession() {
         // Set session save path
         session_save_path($this->cookie);
@@ -206,11 +223,12 @@ class Proxy {
         return session_id();
     }
 
+    /**
+     * Function: Make file paths if necessary
+     * @param $path
+     * @return bool
+     */
     private function makeDirPath($path) {
         return file_exists($path) || mkdir($path, 0777, true);
-    }
-
-    private function test(bool $mandatory = false) {
-        $mandatory = is_null($mandatory) ? false : $mandatory;
     }
 }
